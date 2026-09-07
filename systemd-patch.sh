be3d8a02310372ad57ed9e795698b382000773f0
v0lk3n <volken_haxor@protonmail.com>
2026-09-07 19:44:04 +0200

    Add systemd patch script

---
+++ systemd-patch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl https://github.com/kardebayan/kernel_xiaomi_surya/commit/8df374c3058d83c8335a04fc59946f4397f166ec.patch | git am
+curl https://github.com/kardebayan/kernel_xiaomi_surya/commit/90a68dd273552e8227a89621841de842fc52a531.patch | git am
+curl https://github.com/kardebayan/kernel_xiaomi_surya/commit/17627954089b0428d04d5e4291811d5de150a827.patch | git am
+curl https://github.com/kardebayan/kernel_xiaomi_surya/commit/e0a0ab5a6c45e87ed6bb6d98ee4b84f6eff5b408.patch | git am
+curl https://github.com/kardebayan/kernel_xiaomi_surya/commit/8f850600d1148efe0dff53bf1012d67bac43b7da.patch | git am
+curl https://github.com/kardebayan/kernel_xiaomi_surya/commit/8a1257655420970c5ef09fa6ca7b60dffd73a9ef.patch | git am
+git push
