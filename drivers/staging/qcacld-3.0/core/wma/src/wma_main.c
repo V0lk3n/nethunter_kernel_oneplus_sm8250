ecbcdd0b1f357773ed986ad34f862d63aa0633d6
v0lk3n <volken_haxor@protonmail.com>
2026-09-14 18:36:09 +0200

    qcacld-3.0:Add packet injection and channel fix support

--- drivers/staging/qcacld-3.0/core/wma/src/wma_main.c
+++ drivers/staging/qcacld-3.0/core/wma/src/wma_main.c
@@ -106,6 +106,8 @@
 #include <target_if_direct_buf_rx_api.h>
 #endif
 
+#include "wma_frame_inject.h"
+
 #define WMA_LOG_COMPLETION_TIMER 3000 /* 3 seconds */
 #define WMI_TLV_HEADROOM 128
 
@@ -3394,6 +3396,15 @@ QDF_STATUS wma_open(struct wlan_objmgr_psoc *psoc,
 	wma_register_md_events(wma_handle);
 	wma_register_wlm_stats_events(wma_handle);
 	wma_register_mws_coex_events(wma_handle);
+
+	/* Initialize frame injection queue */
+	qdf_status = wma_init_injection_queue(wma_handle);
+	if (qdf_status != QDF_STATUS_SUCCESS) {
+		WMA_LOGE("%s: Failed to initialize injection queue: %d",
+			 __func__, qdf_status);
+		/* Continue initialization - injection queue failure is not fatal */
+	}
+
 	return QDF_STATUS_SUCCESS;
 
 err_dbglog_init:
@@ -4497,6 +4508,13 @@ QDF_STATUS wma_close(void)
 	pmo_unregister_is_device_in_low_pwr_mode(wma_handle->psoc);
 	pmo_unregister_get_pause_bitmap(wma_handle->psoc);
 	pmo_unregister_pause_bitmap_notifier(wma_handle->psoc);
+
+	/* Deinitialize frame injection queue */
+	qdf_status = wma_deinit_injection_queue(wma_handle);
+	if (qdf_status != QDF_STATUS_SUCCESS) {
+		WMA_LOGE("%s: Failed to deinitialize injection queue: %d",
+			 __func__, qdf_status);
+	}
 
 	tgt_psoc_info = wlan_psoc_get_tgt_if_handle(wma_handle->psoc);
 	init_deinit_free_num_units(wma_handle->psoc, tgt_psoc_info);
